264a9cd8d5e66d25f28b7983fe9708d65dc6e54d
Bobkova Maryna <marih@bk.ru>
2023-11-24 12:47:54 +0100

    add print

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 public class Main {
   public static void main(String[] args) {
     String[] fruits = new String[]{"apple", "orange", "mango"};
-
+    print(fruits);
   }
+   public static void print(String[] arr){
+     System.out.println(Arrays.toString(arr));
+   }
 }
