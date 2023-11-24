52cf6db85db8426ef46204b273fa31ce9a26db73
Martins Groza <mar4inja19@gmail.com>
2023-11-24 12:47:50 +0100

    addPrint

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
-    String[] fruits = new String[]{"apple", "orange", "mango"};
+    public static void main(String[] args) {
+        String[] fruits = new String[]{"apple", "orange", "mango"};
+        print(fruits);
 
-  }
+    }
+
+    public static void print(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
+    }
 }
