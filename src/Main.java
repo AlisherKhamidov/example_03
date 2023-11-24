7eaebb0578bf40a650d033dec14b1c097367d8ea
Vitaliy Kornienko <vit_ko@ukr.net>
2023-11-24 12:46:42 +0100

    add and upate print

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         String[] fruits = new String[]{"apple", "orange", "mango"};
+    }
 
-        for (String fruit : fruits) {
+    public static void print(String[] arr) {
+        for (String fruit : arr) {
             System.out.println(fruit);
         }
     }
