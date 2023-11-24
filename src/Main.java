e6c93cd65a639a7131d243c0c2802eaaba16694c
Vitaliy Kornienko <vit_ko@ukr.net>
2023-11-24 12:47:16 +0100

    upate main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String[] fruits = new String[]{"apple", "orange", "mango"};
+        print(fruits);
     }
 
     public static void print(String[] arr) {
