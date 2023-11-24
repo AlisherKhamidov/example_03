ea8516665060739c65ea137e57115851404e0052
Vitaliy Kornienko <vit_ko@ukr.net>
2023-11-24 12:41:58 +0100

    add print in console

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-import java.util.Arrays;
-
 public class Main {
-  public static void main(String[] args) {
-    String[] fruits = new String[]{"apple", "orange", "mango"};
+    public static void main(String[] args) {
+        String[] fruits = new String[]{"apple", "orange", "mango"};
 
-  }
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
 }
